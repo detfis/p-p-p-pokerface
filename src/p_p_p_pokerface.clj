361f8fc0859124d4e1714e89e698968966f846a1
Kamil Pluta <kamil@skimlinks.com>
2015-03-24 14:46:38 +0000

    exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,50 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
+(defn chain-of-five-numbers [sequence]
+  (let [[first_number _] sequence]
+    (= (range first_number (+ first_number 5)) sequence)))
+  
+
+
 (defn rank [card]
-  nil)
+  (let[[frst _] card]
+    (if (Character/isDigit frst)
+      (Integer/valueOf (str frst))
+      (replacements frst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ scnd] card]
+    (str scnd)))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 2)) 
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 3)) 
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 4)) 
 
 (defn flush? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map suit hand)))) 5)) 
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (seq (sort (vals (frequencies (map rank hand)))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= [1 2 2] (seq (sort (vals (frequencies (map rank hand)))))))
 
 (defn straight? [hand]
-  nil)
-
+  (let [values (seq (sort (vals (frequencies (map rank hand)))))
+        values-with-lower-ace (if (= (last values) 14)
+                                (sort (assoc values 1 14))
+                                values)]
+    (or (chain-of-five-numbers values) (chain-of-five-numbers values-with-lower-ace))))
+    
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
   nil)
